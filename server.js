20c9aa35f7a65cf481e92e1ded76a370b684ed82
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-11 22:37:31 +0300

    tickets are returning by GET request

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-useless-return */
 /* eslint-disable no-unused-vars */
 const http = require('http');
 const Koa = require('koa');
@@ -5,19 +6,55 @@ const koaBody = require('koa-body');
 
 const app = new Koa();
 
+const ticket1 = {
+  id: 1,
+  name: 'ticket1',
+  status: false,
+  created: '11.03.2021 21:36',
+};
+
+const ticket2 = {
+  id: 2,
+  name: 'ticket2',
+  status: false,
+  created: '11.03.2021 21:36',
+};
+
+const ticket3 = {
+  id: 3,
+  name: 'ticket3',
+  status: true,
+  created: '11.03.2021 21:36',
+};
+
+const tickets = [ticket1, ticket2, ticket3];
+
 app.use(koaBody({
   urlencoded: true,
   multipart: true,
 }));
 
 app.use(async (ctx) => {
+  const { method } = ctx.request.query;
+  console.log();
+
   ctx.response.set({
     'Access-Control-Allow-Origin': '*',
   });
 
-  console.log(ctx.request.querystring);
+  console.log(ctx.request.query);
 
-  ctx.response.body = ctx.request.querystring;
+  console.log(method);
+
+  switch (method) {
+    case 'allTickets':
+      ctx.response.body = tickets;
+      return;
+    // TODO: обработка остальных методов
+    default:
+      ctx.response.status = 404;
+      return;
+  }
 });
 
 const port = process.env.PORT || 7070;
