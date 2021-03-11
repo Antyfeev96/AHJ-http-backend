3eac1789979806a95157c3834dde74d4f1bfe671
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-11 18:35:45 +0300

    fs: empty added in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -26,15 +26,9 @@ const ESLintPlugin = require('eslint-webpack-plugin');
  */
 
 module.exports = {
-  // webpack: (config, { isServer }) => {
-  //   // Fixes npm packages that depend on `fs` module
-  //   if (!isServer) {
-  //     config.node = {
-  //       fs: 'empty',
-  //       net: 'empty',
-  //     };
-  //   }
-  // },
+  node: {
+    fs: "empty"
+  },
 
   resolve: {
     fallback: {
