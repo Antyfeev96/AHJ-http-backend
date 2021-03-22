f1916c6469f171771aedd6b3717d68c5f23fc3f6
Dmitri Antyfeev <60137521+Antyfeev96@users.noreply.github.com>
2021-03-22 15:43:20 +0300

    Adding and deleting tickets completed

--- server.js
+++ server.js
@@ -10,50 +10,7 @@ const { formatterDate } = require('./src/js/formatterDate');
 
 const app = new Koa();
 
-const tickets = [
-  {
-    id: 1,
-    name: 'Посеять хлеб',
-    status: true,
-    created: '09.03.2021 16:11',
-  },
-  {
-    id: 2,
-    name: 'Сделать этот макет',
-    status: false,
-    created: '09.03.2021 13:55',
-  },
-  {
-    id: 3,
-    name: 'Преисполниться в своём познании настолько, что как-будто я уже 100 триллионов миллиардов проживаю',
-    status: false,
-    created: '11.03.2021 21:36',
-  },
-];
-
-const fullTickets = [
-  {
-    id: 1,
-    name: 'Посеять хлеб',
-    description: 'Посеять хлеб',
-    status: true,
-    created: '09.03.2021 16:11',
-  },
-  {
-    id: 2,
-    name: 'Сделать этот макет',
-    description: 'Сделать backend',
-    status: false,
-    created: '09.03.2021 13:55',
-  },
-  {
-    id: 3,
-    name: 'Преисполниться в своём познании настолько, что как-будто я уже 100 триллионов миллиардов проживаю',
-    description: 'на триллионах и триллионах такие-же планет, понимаешь, как эта Земля, мне уже этот мир абсолютно понятен',
-    status: false,
-    created: '11.03.2021 21:36',
-  },
-];
+const tickets = [];
 
 app.use(koaBody({
   urlencoded: true,
@@ -80,13 +37,6 @@ app.use(async (ctx) => {
       tickets.push({
         id: tickets.length + 1,
         name: shorttext,
-        status: false,
-        created: formatterDate(),
-      });
-
-      fullTickets.push({
-        id: tickets.length + 1,
-        name: shorttext,
         description: fulltext,
         status: false,
         created: formatterDate(),
@@ -94,11 +44,17 @@ app.use(async (ctx) => {
 
       ctx.response.body = tickets[tickets.length - 1];
       return;
-    case 'deleteById':
+    case 'editTicket':
+      tickets.find((ticket) => ticket.id === +id).name = shorttext;
+      tickets.find((ticket) => ticket.id === +id).description = fulltext;
       ctx.response.body = tickets.find((ticket) => ticket.id === +id);
       return;
+    case 'deleteById':
+      tickets.splice(tickets.findIndex((ticket) => ticket.id === +id), 1);
+      ctx.response.body = tickets;
+      return;
     case 'ticketById':
-      ctx.response.body = fullTickets.find((ticket) => ticket.id === +id);
+      ctx.response.body = tickets.find((ticket) => ticket.id === +id);
       return;
     default:
       ctx.response.status = 404;
